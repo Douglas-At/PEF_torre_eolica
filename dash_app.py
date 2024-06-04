77ee9212442d9c6113cc16c926e0a41d8d402863
Douglas-At <douglasmasouza7@gmail.com>
2024-06-04 01:26:08 -0300

    pequenas mudanças no dash

--- dash_app.py
+++ dash_app.py
@@ -11,15 +11,12 @@ app.layout = html.Div([
     html.Div([
         html.Label('Tamanho das Pás [m]'),
         dcc.Slider(id='blade-slider',min=5,max=30,step=1,value=15,marks={i: str(i) for i in range(5, 31)}),
-
         html.Label('Altura da Torre [m]'),
         dcc.Slider(id='tower-slider',min=20,max=200,step=10,value=70,marks={i: str(i) for i in range(20, 201, 10)}),
-
         html.Div([
             dcc.Graph(id='turbine-plot'),
             dcc.Graph(id='wind-plot'),
         ]),
-
     ], style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'top'}),
     html.Div([
         html.H3('Diagramas de Esforços na Torre', style={'textAlign': 'center'}),
@@ -45,12 +42,10 @@ def update_plots(blade_length, tower_height):
     wc = WindCalculator(blade_length=blade_length,tower_height=tower_height, cd=0.82,wind_speed=10)
     turbine_fig = wc.plot_turbine()
     wind_fig = wc.plot_wind_behavior()
-    #n_fig = wc.plot_n()  
+    n_fig = wc.plot_n()  
     v_fig = wc.plot_v() 
     m_fig = wc.plot_m()
-    n_fig = go.Figure()
     
-
     return turbine_fig, wind_fig, n_fig, v_fig, m_fig
 
 if __name__ == '__main__':
