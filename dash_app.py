2104152704c2f9de650bc69e878e44033513fb3b
Douglas-At <douglasmasouza7@gmail.com>
2024-06-03 23:19:44 -0300

    adicionando 2 dos diagramas do poste

--- dash_app.py
+++ dash_app.py
@@ -1,52 +1,57 @@
-import dash_app
-from dash_app import dcc, html
+import dash
+from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import numpy as np
 from wind_tower import WindCalculator
 
-app = dash_app.Dash(__name__)
-
-
+app = dash.Dash(__name__)
 
 app.layout = html.Div([
     html.Div([
-        html.Label('Blade Length'),
-        dcc.Slider(
-            id='blade-slider',
-            min=5,
-            max=30,
-            step=1,
-            value=15,
-            marks={i: str(i) for i in range(5, 31)}
-        ),
-        html.Label('Tower Height'),
-        dcc.Slider(
-            id='tower-slider',
-            min=20,
-            max=100,
-            step=5,
-            value=70,
-            marks={i: str(i) for i in range(20, 101, 5)}
-        ),
-    ], style={'margin-bottom': '20px'}),
+        html.Label('Tamanho das Pás [m]'),
+        dcc.Slider(id='blade-slider',min=5,max=30,step=1,value=15,marks={i: str(i) for i in range(5, 31)}),
+
+        html.Label('Altura da Torre [m]'),
+        dcc.Slider(id='tower-slider',min=20,max=200,step=10,value=70,marks={i: str(i) for i in range(20, 201, 10)}),
+
+        html.Div([
+            dcc.Graph(id='turbine-plot'),
+            dcc.Graph(id='wind-plot'),
+        ]),
+
+    ], style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'top'}),
     html.Div([
-        dcc.Graph(id='turbine-plot'),
-        dcc.Graph(id='wind-plot'),
-    ]),
-])
+        html.H3('Diagramas de Esforços na Torre', style={'textAlign': 'center'}),
+        dcc.Graph(id='n-plot'),
+        dcc.Graph(id='v-plot'),
+        dcc.Graph(id='m-plot'),
+    ], style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'top'}),
+], style={'display': 'flex', 'flex-direction': 'row'})
+
+
 
 @app.callback(
     [Output('turbine-plot', 'figure'),
-     Output('wind-plot', 'figure')],
+     Output('wind-plot', 'figure'),
+     Output('n-plot', 'figure'),
+     Output('v-plot', 'figure'),
+     Output('m-plot', 'figure')],
     [Input('blade-slider', 'value'),
      Input('tower-slider', 'value')]
 )
 def update_plots(blade_length, tower_height):
-    wc = WindCalculator(blade_length,tower_height)
-    turbine_fig = wc.plot_turbine(blade_length, tower_height)
-    wind_fig = wc.plot_wind_behavior(tower_height, blade_length)
-    return turbine_fig, wind_fig
+    
+    wc = WindCalculator(blade_length=blade_length,tower_height=tower_height, cd=0.82,wind_speed=10)
+    turbine_fig = wc.plot_turbine()
+    wind_fig = wc.plot_wind_behavior()
+    #n_fig = wc.plot_n()  
+    v_fig = wc.plot_v() 
+    m_fig = wc.plot_m()
+    n_fig = go.Figure()
+    
+
+    return turbine_fig, wind_fig, n_fig, v_fig, m_fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- wind_tower.py
+++ wind_tower.py
@@ -1,15 +1,15 @@
-
 import plotly.graph_objs as go
+from scipy.constants import g 
 import numpy as np
 
 
 class WindCalculator:
-    def __init__(self,blade_length, tower_height):
+    def __init__(self,blade_length, tower_height,cd,wind_speed):
         self.blade_length = blade_length
         self.tower_height = tower_height
-
-    def wind_function(self,y):
-        return np.log(y + 1) * 2  
+        self.wind_speed = wind_speed
+        self.cd = cd
+        self.g = g
 
     def plot_turbine(self):
         angles = np.linspace(0, 2 * np.pi, 4)[:-1]
@@ -32,18 +32,98 @@ class WindCalculator:
         )
 
         return {'data': traces, 'layout': layout}
+    
+    def wind_function(self):
+        return self.wind_speed
 
     def plot_wind_behavior(self):
         y_values = np.linspace(0, self.tower_height + self.blade_length, 500)
-        wind_speeds = self.wind_function(y_values)
+        
+        wind_speeds = np.full_like(y_values, self.wind_function())
+        
 
         trace = go.Scatter(x=wind_speeds, y=y_values, mode='lines', line=dict(color='green'))
 
         layout = go.Layout(
-            title='Wind Speed vs. Height',
-            xaxis=dict(title='Wind Speed'),
-            yaxis=dict(title='Height'),
+            title='Velocida do Vento x Altura',
+            xaxis=dict(title='Velocidade do Vento [m/s]'),
+            yaxis=dict(title='Altura [m]'),
             height=600
         )
-
+        
         return {'data': [trace], 'layout': layout}
+    
+    def function_v(self,y):
+        """
+        calculo de v no diagrama
+        V = cd*g*v²(y-H)/2 # anexar resolução no papel 
+        """
+        return (self.cd*self.g*self.wind_function()**2 * (y - self.tower_height)) / 2
+
+    def plot_v(self):
+        y_values = np.linspace(0, self.tower_height, 500)
+        v_values = [self.function_v(y) for y in y_values]
+        max_abs_v = max(abs(min(v_values)), abs(max(v_values)))
+
+        traces = [go.Scatter(x=[0, 0], y=[0, self.tower_height], mode='lines', line=dict(color='black', width=5), name='Tower')]
+        traces.append(go.Scatter(x=v_values, y=y_values, mode='lines', line=dict(color='red')))
+
+        filled_area = go.Scatter(x=v_values + [0],y=y_values.tolist() + [y_values[0]],fill='tozerox',
+                                 fillpattern=dict(shape='\\'),mode='none' ,fillcolor='rgba(255, 0, 0, 0.3)')
+
+        layout = go.Layout(
+            title='Diagrama de Forças Cortantes (V)',
+            xaxis=dict(title='Força Cortante (V) [N]',showgrid=True,gridcolor='lightgrey',range=[-max_abs_v, max_abs_v]),
+            yaxis=dict(title='Altura [m]'),
+            height=600,
+            showlegend=False
+        )
+
+        return {'data': [filled_area] + traces, 'layout': layout}
+
+    def function_m(self,y):
+        """
+        calculo de m no diagrama
+        m = cd*g*v²(H²/4 - Hy/2 + y²/4)
+        """
+        return self.cd * self.g * self.wind_function()**2 * (self.tower_height**2 / 4 - self.tower_height * y / 2 + y**2 / 4)
+
+
+    def plot_m(self):
+        y_values = np.linspace(0, self.tower_height, 500)
+        m_values = [self.function_m(y) for y in y_values]
+        max_abs_m = max(abs(min(m_values)), abs(max(m_values)))
+
+        traces = [
+            # o poste
+            go.Scatter(x=[0, 0], y=[0, self.tower_height], mode='lines', line=dict(color='black', width=5), name='Tower'),
+            go.Scatter(x=m_values, y=y_values, mode='lines', line=dict(color='blue'))
+        ]
+
+        
+        filled_area = go.Scatter(
+            x=m_values + [0],
+            y=y_values.tolist() + [y_values[0]],
+            fill='tozerox',
+            fillpattern=dict(shape='/', fgcolor='rgba(0, 0, 255, 0.3)'),  
+            mode='none'
+        )
+
+        layout = go.Layout(
+            title='Diagrama de Momentos Fletores (M)',
+            xaxis=dict(
+                title='Diagrama de Momentos Fletores (M) [Nm]',
+                showgrid=True,
+                gridcolor='lightgrey',
+                range=[-max_abs_m, max_abs_m]
+            ),
+            yaxis=dict(
+                title='Altura [m]'
+            ),
+            height=600,
+            showlegend=False
+        )
+
+        return {'data': [filled_area] + traces, 'layout': layout}
+
+
